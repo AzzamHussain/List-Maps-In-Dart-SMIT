17bb1016881b53419e355941c899abb9d545a8e5
AzzamHussain <azzamhussain70@gmail.com>
2024-01-27 01:23:56 +0500

    Assignment02

---
+++ Qno20.dart
@@ -0,0 +1,14 @@
+// Q.20: Create a map named "car" with the following key-value pairs: "brand" as
+// "Toyota", "color" as "Red", "isSedan" as true. Write Dart code to check if the
+// car is a sedan and red in color. Print "Match" if both conditions are true,
+// otherwise print "No match".
+void main(){
+  Map car={"brand":"Toyota","color":"Red","isSedan":true};
+  bool sedan=car["isSedan"];
+  String color=car["color"];
+  if (sedan==true && color=="Red") {
+    print("Match");
+  } else {
+    print("NO match");
+  }
+}
